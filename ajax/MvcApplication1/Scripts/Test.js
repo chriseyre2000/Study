e8409f2006e0072737737a405589fa50f3585179
Christopher Eyre <chriseyre2000@gmail.com>
2013-06-02 08:51:42 +0100

    This is a simple ajax demo

---
+++ ajax/MvcApplication1/Scripts/Test.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+
+    $("#Upper").html("This is a test");
+
+    $("[data-test]").click(function (a) {
+        $("#Upper").load($(a.target).attr("data-test"))
+    }); 
+});
