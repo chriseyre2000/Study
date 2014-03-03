71e293889b58f21425f454aaf617d2c4a1956b01
Christopher Eyre <chriseyre2000@gmail.com>
2014-03-03 22:03:38 +0000

    Nancy and d3

---
+++ d3/NancyD3/D3DataModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace NancyD3
+{
+
+    public class Data
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+
+    public class D3DataModule : NancyModule
+    { 
+        public D3DataModule()
+        {
+            Get["data"] = x => 
+                {   
+                    return new [] {
+                        new Data { x = 1, y = 5 },
+                        new Data { x = 20, y = 20 },
+                        new Data { x = 40, y = 10 },
+                        new Data { x = 60, y = 40 },
+                        new Data { x = 80, y = 5 },
+                        new Data { x = 100, y = 60 }
+                    }; 
+                };
+
+            Get["/"] = x =>  View["Index"];
+        }
+    }
+}
---
+++ d3/NancyD3/content/temp.js
@@ -0,0 +1,37 @@
+﻿
+d3.json('data', function (data) {
+    
+    var svg = d3.select("svg");
+
+    svg.append("rect").attr("x", 0)
+                      .attr("y", 0)
+                      .attr("width", 100)
+                      .attr("height", 80)
+                      .attr("fill", "yellow")
+    
+    //The data for our line
+    var lineData = data;
+
+    var xscale = d3.scale.linear();
+    xscale.domain([0, 100]);
+    xscale.range([100, 0]);
+
+    var yscale = d3.scale.linear();
+    yscale.domain([5,70 ]);
+    yscale.range([0,50])
+    
+    //This is the accessor function we talked about above
+    var lineFunction = d3.svg.line()
+                             .x(function (d) { return xscale(d.x); })
+                             .y(function (d) { return d.y; })
+                             .interpolate("linear");
+
+    //The line SVG Path we draw
+    var lineGraph = svg.append("path")
+                       .attr("d", lineFunction(lineData))
+                       .attr("stroke", "green")
+                       .attr("stroke-width", 2)
+                       .attr("fill", "none");
+});
+
+
